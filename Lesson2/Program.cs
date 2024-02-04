c8b37aec77949cabc953672fe2f06a8d12fac1e7
Shkut403 <158722883+Shkut403@users.noreply.github.com>
2024-02-04 22:34:23 +0700

    Add files via upload

---
+++ Lesson2/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Lesson2
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите год (число должно быть больше 0 и меньше 30000):");
+            string input = Console.ReadLine();
+            int year;
+
+            if (int.TryParse(input, out year) && year > 0 && year < 30000)
+            {
+                if (IsLeapYear(year))
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Введенное значение не является допустимым номером года (должно быть больше 0 и меньше 30000).");
+            }
+        }
+
+        static bool IsLeapYear(int year)
+        {
+            return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+        }
+    }
+}
