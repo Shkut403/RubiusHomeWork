4026fd11b7de40fa5e6421580912e5b37fe6fea9
Shkut403 <158722883+Shkut403@users.noreply.github.com>
2024-02-04 22:36:11 +0700

    Add files via upload

---
+++ Lesson1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Lesson1
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Why on Earth I study C#!");
+            Console.Clear();
+            Console.WriteLine("Hello, World!");
+        }
+    }   
+}
