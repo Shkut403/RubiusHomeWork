cef366388ad6a7f83eb84e3e10d8067d681cd7af
Shkut403 <158722883+Shkut403@users.noreply.github.com>
2024-02-11 19:52:31 +0700

    Update Visitor.cs

--- Lesson4/Lesson4/Visitor.cs
+++ Lesson4/Lesson4/Visitor.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Lesson4
+﻿namespace Lesson4
 {
     public class Visitor
     {
@@ -16,7 +10,5 @@ namespace Lesson4
             Name = name;
             Cafe = cafe;
         }
-
-        // Другие методы посетителя
     }
 }
