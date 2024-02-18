9e5c99d27fdc9e6c2ed3aab10748f1e11bd7bec5
DESKTOP-JR8SC8U\3DS_Asus <iliakutuzov403@gmail.com>
2024-02-18 21:09:08 +0700

    HW-5
    I prepared my homwork 5

---
+++ Lesson5/Lesson5/Doctor.cs
@@ -0,0 +1,25 @@
+﻿namespace Lesson5
+{
+    using System;
+    class Doctor
+    {
+        public string Name { get; set; }
+        public string Specialization { get; set; }
+        public int Experience { get; set; }
+
+        public Doctor(string name, string specialization, int experience)
+        {
+            Name = name;
+            Specialization = specialization;
+            Experience = experience;
+        }
+
+        public void Cure(Patient patient)
+        {
+            Random random = new Random();
+            patient.Status = random.Next(2) == 0 ? "Healthy" : "Sick";
+            Console.WriteLine($"{Name} ({Specialization}) treated patient {patient.Name}.");
+        }
+    }
+
+}
---
+++ Lesson5/Lesson5/Patient.cs
@@ -0,0 +1,19 @@
+﻿namespace Lesson5
+{
+    using System;
+    class Patient
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public string Status { get; set; }
+
+        public Patient(string name, int age, string address, string status)
+        {
+            Name = name;
+            Age = age;
+            Address = address;
+            Status = status;
+        }
+    }
+}
