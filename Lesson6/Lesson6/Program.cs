ff6f9277e2cc71c97d1a405114f0fbad856c08b4
DESKTOP-JR8SC8U\3DS_Asus <iliakutuzov403@gmail.com>
2024-02-18 21:56:32 +0700

    HW-6
    I prepared HW-6

---
+++ Lesson6/Lesson6/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Lesson6
+{
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создание и использование Stack<int>
+            Stack<int> intStack = new Stack<int>();
+            intStack.Push(1);
+            intStack.Push(2);
+            Console.WriteLine($"Верхний элемент intStack: {intStack.Peek()}");
+            Console.WriteLine($"Удален элемент из intStack: {intStack.Pop()}");
+            Console.WriteLine($"Стек intStack пуст? {intStack.IsEmpty()}");
+
+            // Создание и использование Stack<string>
+            Stack<string> stringStack = new Stack<string>();
+            stringStack.Push("Hello");
+            stringStack.Push("World");
+            Console.WriteLine($"Верхний элемент stringStack: {stringStack.Peek()}");
+            stringStack.Clear();
+            Console.WriteLine($"Стек stringStack пуст? {stringStack.IsEmpty()}");
+
+            // Создание и использование Stack<double>
+            Stack<double> doubleStack = new Stack<double>();
+            doubleStack.Push(1.1);
+            doubleStack.Push(2.2);
+            Console.WriteLine($"Верхний элемент doubleStack: {doubleStack.Peek()}");
+            Console.WriteLine($"Удален элемент из doubleStack: {doubleStack.Pop()}");
+            Console.WriteLine($"Стек doubleStack пуст? {doubleStack.IsEmpty()}");
+        }
+    }
+}
