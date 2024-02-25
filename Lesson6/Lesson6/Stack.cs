f16220a7afda8329fe853b95ab3c30cc57d0891f
DESKTOP-JR8SC8U\3DS_Asus <iliakutuzov403@gmail.com>
2024-02-25 09:57:09 +0700

    1

--- Lesson6/Lesson6/Stack.cs
+++ Lesson6/Lesson6/Stack.cs
@@ -4,39 +4,32 @@
     public class Stack<T>
     {
         private List<T> items = new List<T>();
-
         public void Push(T item)
         {
             items.Add(item);
         }
-
         public T Pop()
         {
             if (IsEmpty())
             {
                 throw new InvalidOperationException("Стек пуст");
             }
-
             var item = items[^1];
             items.RemoveAt(items.Count - 1);
             return item;
         }
-
         public T Peek()
         {
             if (IsEmpty())
             {
                 throw new InvalidOperationException("Стек пуст");
             }
-
             return items[^1];
         }
-
         public bool IsEmpty()
         {
             return items.Count == 0;
         }
-
         public void Clear()
         {
             items.Clear();
