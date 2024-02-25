195b37032100146e5476584a301b44817c84d44e
DESKTOP-JR8SC8U\3DS_Asus <iliakutuzov403@gmail.com>
2024-02-25 10:13:48 +0700

    HW-7
    Made HW-7

---
+++ Lesson6/Lesson6 Test/Test.cs
@@ -0,0 +1,37 @@
+﻿namespace Lesson6
+{
+    using Xunit;
+    public class StackTests
+    {
+        [Fact]
+        public void PushAndPeek_SingleItem_ReturnsItem()
+        {
+            var stack = new Stack<int>();
+            stack.Push(1);
+            var result = stack.Peek();
+            Assert.Equal(1, result);
+        }
+        [Fact]
+        public void Pop_EmptyStack_ThrowsInvalidOperationException()
+        {
+            var stack = new Stack<int>();
+            Assert.Throws<InvalidOperationException>(() => stack.Pop());
+        }
+        [Fact]
+        public void IsEmpty_EmptyStack_ReturnsTrue()
+        {
+            var stack = new Stack<int>();
+            var result = stack.IsEmpty();
+            Assert.True(result);
+        }
+        [Fact]
+        public void Clear_NonEmptyStack_EmptiesTheStack()
+        {
+            var stack = new Stack<int>();
+            stack.Push(1);
+            stack.Clear();
+            var result = stack.IsEmpty();
+            Assert.True(result);
+        }
+    }
+}
