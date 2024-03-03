3b252e782fad5ac46e078b633005636d302161ac
DESKTOP-JR8SC8U\3DS_Asus <iliakutuzov403@gmail.com>
2024-03-03 14:37:14 +0700

    HW-8 (task-2)
    Prepared homework

---
+++ Lesson8 (task 2)/Lesson8 (task 2)/Ping.cs
@@ -0,0 +1,24 @@
+﻿namespace Lesson8__task_2_
+{
+    using System;
+
+    public class Ping
+    {
+        public event Action PingSent;
+        private Random random = new Random();
+
+        public void SendPing()
+        {
+            if (random.Next(0, 10) < 8) // 80% шанс отправить пинг
+            {
+                Console.WriteLine("Ping получил Pong");
+                PingSent?.Invoke();
+            }
+            else
+            {
+                Console.WriteLine("Ping промахнулся! Победил Pong");
+            }
+        }
+    }
+
+}
---
+++ Lesson8 (task 2)/Lesson8 (task 2)/Pong.cs
@@ -0,0 +1,22 @@
+﻿namespace Lesson8__task_2_
+{
+    public class Pong
+    {
+        public event Action PongSent;
+        private Random random = new Random();
+
+        public void OnPingReceived()
+        {
+            if (random.Next(0, 10) < 8) // 80% шанс ответить понг
+            {
+                Console.WriteLine("Pong получил Ping");
+                PongSent?.Invoke();
+            }
+            else
+            {
+                Console.WriteLine("Pong промахнулся! Победил Ping");
+            }
+        }
+    }
+
+}
