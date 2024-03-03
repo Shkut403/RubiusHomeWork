a1b6bfd9ec9a34154017ee1dff717aad0740035d
DESKTOP-JR8SC8U\3DS_Asus <iliakutuzov403@gmail.com>
2024-03-03 14:05:42 +0700

    HW-8 (task1)
    Prepared homwork

---
+++ Lesson8/Lesson8/Program.cs
@@ -0,0 +1,15 @@
+﻿using Lesson8;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Counter counter = new Counter();
+        Handler1 handler1 = new Handler1();
+        Handler2 handler2 = new Handler2();
+
+        counter.OnCount += handler1.Message;
+        counter.OnCount += handler2.Message;
+
+        counter.Count();
+    }
+}
