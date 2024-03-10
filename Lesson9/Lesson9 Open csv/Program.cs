9008f86507c30f639331ec3a40e4c4cca593ffc2
DESKTOP-JR8SC8U\3DS_Asus <iliakutuzov403@gmail.com>
2024-03-10 21:29:28 +0700

    HW-9
    Prepared HW

---
+++ Lesson9/Lesson9 Open csv/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Lesson9
+{ 
+    class Program
+    {
+        static void Main()
+        {
+            string appDataFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Lesson12Homework.txt");
+
+            string csvFilePath = File.ReadAllText(appDataFilePath);
+
+            List<FileInfoRecord> records = new List<FileInfoRecord>();
+            using (StreamReader sr = new StreamReader(csvFilePath))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    var parts = line.Split('\t');
+                    records.Add(new FileInfoRecord(parts[0], parts[1], DateTime.Parse(parts[2])));
+                }
+            }
+
+            foreach (var record in records.OrderBy(r => r.LastModified))
+            {
+                Console.WriteLine($"{record.Type}\t{record.Name}\t{record.LastModified}");
+            }
+
+            File.Delete(appDataFilePath);
+        }
+    }
+}
---
+++ Lesson9/Lesson9/Zip csv.cs
@@ -0,0 +1,47 @@
+﻿using Lesson9;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+
+namespace Lesson9
+{ 
+    class Program
+    {
+        static void Main()
+        {
+            string archivePath = "C:/Users/3DS_Asus/source/archive.zip";  // Указать путь к архиву
+            string extractPath = Path.Combine(Directory.GetCurrentDirectory(), "extracted");
+            string csvFilePath = Path.Combine(Directory.GetCurrentDirectory(), "files.csv");
+            string appDataFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Lesson12Homework.txt");
+
+            ZipFile.ExtractToDirectory(archivePath, extractPath);
+
+            List<FileInfoRecord> records = new List<FileInfoRecord>();
+            foreach (var directory in Directory.GetDirectories(extractPath))
+            {
+                var dirInfo = new DirectoryInfo(directory);
+                records.Add(new FileInfoRecord("Folder", dirInfo.Name, dirInfo.LastWriteTime));
+            }
+
+            foreach (var file in Directory.GetFiles(extractPath))
+            {
+                var fileInfo = new FileInfo(file);
+                records.Add(new FileInfoRecord("File", fileInfo.Name, fileInfo.LastWriteTime));
+            }
+
+            using (StreamWriter sw = new StreamWriter(csvFilePath))
+            {
+                foreach (var record in records)
+                {
+                    sw.WriteLine($"{record.Type}\t{record.Name}\t{record.LastModified}");
+                }
+            }
+
+            Directory.Delete(extractPath, true);
+
+            File.WriteAllText(appDataFilePath, csvFilePath);
+        }
+    }
+}
